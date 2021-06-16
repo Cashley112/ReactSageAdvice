4ea31010b4041e9615dd9aedbc99e4db323b36fc
Connor Ashley <ashleycm@vcu.edu>
2021-06-16 12:14:21 -0400

    render a number of buttons from array in default props and set style equal to an object constructed from that array using .map

---
+++ src/ButtonList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './ButtonList.css';
+
+class ButtonList extends Component {
+    static defaultProps = {
+        colors: ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"]
+    };
+    render() {
+        return (
+            <div className="ButtonList">
+                {this.props.colors.map(c => {
+                    const colorObj = { backgroundColor: c };
+                    return <button style={colorObj}>Click On Me!</button>
+                })}
+            </div>
+        )
+    }
+}
+
+export default ButtonList;
