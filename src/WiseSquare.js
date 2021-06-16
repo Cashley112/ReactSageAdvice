f495483fd69129d36da8874f746d36df7e578586
Connor Ashley <ashleycm@vcu.edu>
2021-06-16 12:01:54 -0400

    moved messages array into defaultProps so they can be reconfigured. Called .bind to adjust the context of this

--- src/WiseSquare.js
+++ src/WiseSquare.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react'
 import './WiseSquare.css';
 
 class WiseSquare extends Component {
-    handleMouseEnter() {
-        let messages = [
+    constructor(props) {
+        super(props);
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    }
+
+    static defaultProps = {
+        messages: [
             "A smooth sea never made a skilled sailor.",
             "The comeback is always greater than the setback.",
-            "Educating the mind without educating the heart is no education at all"
+            "Educating the mind without educating the heart is no education at all."
         ]
+    }
+    handleMouseEnter() {
+        let { messages } = this.props;
         let rIndex = Math.floor(Math.random() * messages.length);
         console.log(messages[rIndex]);
     }
+
     render () {
         return (
             <div>
