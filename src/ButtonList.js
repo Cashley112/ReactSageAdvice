28a5350f78a416810c8febc264c85a4e5033ad07
Connor Ashley <ashleycm@vcu.edu>
2021-06-16 12:32:15 -0400

    added event handler to change color of surrounding div to the color of the button when clicked

--- src/ButtonList.js
+++ src/ButtonList.js
@@ -5,12 +5,22 @@ class ButtonList extends Component {
     static defaultProps = {
         colors: ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"]
     };
+
+    constructor(props){
+        super(props);
+        this.state = { color: 'white' };
+    }
+
+    changeColor(newColor) {
+        this.setState({ color: newColor });
+    }
+
     render() {
         return (
-            <div className="ButtonList">
+            <div className="ButtonList" style={{ backgroundColor: this.state.color }}>
                 {this.props.colors.map(c => {
                     const colorObj = { backgroundColor: c };
-                    return <button style={colorObj}>Click On Me!</button>
+                    return <button style={colorObj} onClick={this.changeColor.bind(this, c)}>Click On Me!</button>
                 })}
             </div>
         )
